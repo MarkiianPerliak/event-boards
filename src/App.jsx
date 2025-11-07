13d4461471a4e34c734463000d54361414fca6f1
MarkiianPerliak <perliak2012@gmail.com>
2025-11-07 23:33:43 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css';
 import data from "./upcoming-events.json"
 import {PageBoard} from "./components/PageBoard.jsx"
+import styled from "styled-components";
 
 export const App = () => {
   console.log(data)
   return (
     <div className="App">
+      <TextNice>24th Core Workds Coalition Conference</TextNice>
       <PageBoard events={data}/>
     </div>
   );
 }
+
+const TextNice = styled.p`
+  background: #d8e8f9;
+  border: 1px solid #9cb6d3;
+  border-radius: 6px;
+  padding: 8px 16px;
+  text-align: center;
+  font-weight: 600;
+  color: #2c3e50;
+  margin-bottom: 20px;
+`;
--- src/components/PageBoard.jsx
+++ src/components/PageBoard.jsx
@@ -1,11 +1,24 @@
 import {Event} from "./Event.jsx"
+import styled from "styled-components";
 
 export const PageBoard = ({events}) => {
     return (
-        <ul className="PageBoard">
+        <List className="PageBoard">
             {events.map(event => {
                 return <Event data={event}/>
             })}
-        </ul>
+        </List>
     )
 }
+
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 24px;
+  padding: 40px;
+  margin: 0;
+  background: #e6ebee;
+  border-radius: 8px;
+  list-style: none;
+`
